524ca06a1c57a4767bb72e0e1a3127b53540b0f9
Alexsus <alexsuslin@inbox.ru>
2022-12-05 22:18:40 +0300

    kata10

---
+++ JS/Geometry Basics: Circle Area in 2D.js
@@ -0,0 +1,8 @@
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+// The Circle class can be seen below:
+
+function circleArea(circle) {
+  return Math.PI * circle.radius ** 2;
+}
