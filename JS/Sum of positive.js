8f79ba32e734296004fe290ace0a63f767e5564e
Alexsus <alexsuslin@inbox.ru>
2022-12-05 22:46:24 +0300

    kata30

---
+++ JS/Sum of positive.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let a = 0;
+  arr.forEach((el) => {
+    if (el > 0) {
+      a += el;
+    }
+  });
+  return a;
+}
